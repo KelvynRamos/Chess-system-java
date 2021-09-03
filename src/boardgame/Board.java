85bac3e9a1aa85bc29a4b4f7ddd94e45cda71b48
Kelvyn Ramos <dike14@gmail.com>
2021-09-02 23:50:32 -0300

    Camada de xadrez e imprimir o tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -28,4 +28,12 @@ public class Board {
 		this.columns = columns;
 	}
 	
+	public Piece piece(int row, int clolumn) {
+		return pieces[row][clolumn];
+	}
+	
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()]; 
+	}
+	
 }
