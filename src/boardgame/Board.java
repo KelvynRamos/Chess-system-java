35e34dee7116cd9e1485f18a17abe46ac1bd79dc
Kelvyn Ramos <dike14@gmail.com>
2021-09-06 13:21:04 -0300

    ajustante exceções e limpando a tela

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -47,7 +47,7 @@ public class Board {
 	
 	public Piece removePiece(Position position) {
 		if (!positionExists(position)) {
-			throw new BoardException("Posição não existente no tabuleiro");
+			throw new BoardException("Posicao nao existente no tabuleiro");
 		}
 		if (piece(position) == null) {
 			return null;
@@ -68,7 +68,7 @@ public class Board {
 	
 	public boolean thereIsAPiece(Position position) {
 		if(!positionExists(position)) {
-			throw new BoardException("Posição não existente no tabuleiro");
+			throw new BoardException("Posicao nao existente no tabuleiro");
 		}
 		return piece(position) != null;
 	}
